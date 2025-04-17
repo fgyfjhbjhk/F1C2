6b332a55e382a94225d823063ec023cb29ae700a
fgyfjhbjhk <2899263992@qq.com>
2025-04-17 22:50:33 +0800

    Add files via upload

---
+++ MenuMapper.java
@@ -0,0 +1,16 @@
+package com.restaurant.mapper;
+
+import com.restaurant.pojo.Menu;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author sdlg
+ * @since 2025-01-02
+ */
+public interface MenuMapper extends BaseMapper<Menu> {
+
+}
